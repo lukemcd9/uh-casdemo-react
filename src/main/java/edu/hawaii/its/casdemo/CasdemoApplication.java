9f475f206d55754ab15acd397c00c9e5558a18da
lukemcd9 <lukemcd9@hawaii.edu>
2024-10-01 16:24:26 -1000

    Upgrade casdemo base to Spring Boot 3 and Spring Security 6

---
+++ src/main/java/edu/hawaii/its/casdemo/CasdemoApplication.java
@@ -0,0 +1,13 @@
+package edu.hawaii.its.casdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/hawaii/its/casdemo/CasdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hawaii.its.casdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
